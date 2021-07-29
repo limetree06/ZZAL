833658b411f3ad5038205c7c26724e7fea0da931
limetree06 <limetree06@unist.ac.kr>
2021-07-30 01:34:17 +0900

    d

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-function refresh() {
-  fetch("http://192.249.18.145:443/refresh", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({}),
-  })
-    .then((res) => {
-      if (res.status === 400) {
-        console.log("fail");
-      } else if (res.status === 200) {
-        console.log("success");
-        document.getElementById("container").innerHTML = "image section";
-      }
-    })
-    .catch((error) => console.log("error", error));
-}
-console.log("refresh");
-
 function search_local() {
   search = document.getElementById("search_local").value;
   image = document.getElementById("getimage");
@@ -49,9 +28,9 @@ function search_local() {
               json[index].img.data.toString("base64");
 
             document.getElementById("container").innerHTML +=
-              "<div> <img width = '400px' height = '300px' src=" +
+              "<div> <img width =300px height =200px src=" +
               imgsrc +
-              "><button ><img src=./resource/love.png width = '40px' height = '30px'> </button></div>";
+              "><button class ='button_love' ><img src=./resource/love.png width ='40px' height = '30px'></button></div>";
             index++;
           }
         });
