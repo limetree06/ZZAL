8180f1b5045528224f502a0186f06eee002ff3c9
limetree06 <limetree06@unist.ac.kr>
2021-07-30 01:13:56 +0900

    from minji

--- script.js
+++ script.js
@@ -74,3 +74,7 @@ function keybroad() {
 function upload() {
   document.getElementById("upload").reset();
 }
+
+function showPopup() {
+  window.open("popup.html", "a", "width=400, height=300, left=100, top=50");
+}
